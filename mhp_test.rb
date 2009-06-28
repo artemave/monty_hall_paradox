60be174796609a7ff8486eacf73c73a4e8fd88b3
artemave <artemave@gmail.com>
2009-06-27 20:32:01 -0400

    got rid of useless complex hash

--- mhp_test.rb
+++ mhp_test.rb
@@ -1,11 +1,9 @@
-require 'rubygems'
-require 'facets'
 require 'pp'
 require './lib/game'
 
-res = Hash.autonew
-res[:door_changed][:won] = 0 # being able to not write this is where perl rules
-res[:door_not_changed][:won] = 0
+res = {}
+res[:door_changed] = 0 # being able to not write this is where perl rules
+res[:door_not_changed] = 0
 
 10000.times do |i|
   game = Game.new
@@ -16,7 +14,7 @@ res[:door_not_changed][:won] = 0
     game.swap_doors
     key = 'door_changed'
   end
-  res[key.intern][:won]+= 1 if game.won?
+  res[key.intern]+= 1 if game.won?
 end
 
 pp res
