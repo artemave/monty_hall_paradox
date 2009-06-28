4083cb536d4fcdc313f2991ac4ccac639ac85d04
artemave <artemave@gmail.com>
2009-06-27 20:35:24 -0400

    keys are now symbols

--- mhp_test.rb
+++ mhp_test.rb
@@ -2,19 +2,19 @@ require 'pp'
 require './lib/game'
 
 res = {}
-res[:door_changed] = 0 # being able to not write this is where perl rules
+res[:door_changed] = 0 # being able not to write this is where perl rules
 res[:door_not_changed] = 0
 
 10000.times do |i|
   game = Game.new
   game.pick_door
   game.open_false_door
-  key = 'door_not_changed'
+  key = :door_not_changed
   if i % 2 == 0
     game.swap_doors
-    key = 'door_changed'
+    key = :door_changed
   end
-  res[key.intern]+= 1 if game.won?
+  res[key]+= 1 if game.won?
 end
 
 pp res
